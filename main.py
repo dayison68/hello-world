13c7d3c57e94d8e41a8391e17b35356187263a2f
dayison68 <briangillock@gmail.com>
2025-03-26 06:37:12 -0400

    Add command-line support for custom name

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def say_hello():
-    print("Hello, world!")
+import sys
 
-say_hello()
+def say_hello(name="world"):
+    print(f"Hello, {name}!")
+
+if __name__ == "__main__":
+        if len(sys.argv) > 1:
+            say_hello(sys.argv[1])
+        else:
+            say_hello()
